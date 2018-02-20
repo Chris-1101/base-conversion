446fcfa4e0b78a5b5ce61a637a8cc02cf0aa8631
Chris <Chris-1101@users.noreply.github.com>
2018-02-20 21:07:53 +0100

    Image button template, text box, embedded fonts

--- CMB.BaseConversion/CMB.BaseConversion.WPF/MainWindow.xaml.cs
+++ CMB.BaseConversion/CMB.BaseConversion.WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,34 @@ namespace CMB.BaseConversion.WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Window Drag Control
+        private void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        // Window Controls
+        private void MinimiseWin_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void CloseWin_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Side Bar Tabs
+        private void ResetTab_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("<Placeholder> Reset Clicked! </Placeholder>");
+        }
+
+        private void ExitTab_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
