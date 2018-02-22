cf79ac4c4b0eafdd30c28db3ace41fc53f580660
Chris <Chris-1101@users.noreply.github.com>
2018-02-22 23:42:24 +0100

    Implemented base conversion class

---
+++ CMB.BaseConversion/CMB.BaseConversion.WPF/BaseConversion.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CMB.BaseConversion
+{
+    class BaseConversion
+    {
+        /// <summary>Converts a number from one base to another.</summary>
+        public string Convert(string num, int fromRadix, int toRadix)
+        {
+            // Check if the number is negative
+            bool isNegative = num.Contains("-");
+
+            if (isNegative)
+                num = num.Remove(0, 1);
+            
+            // Under these conditions, nothing changes
+            if (Double.TryParse(num, out double dbl) && (dbl == 0.0 || dbl == 1.0) || fromRadix == toRadix)
+                return isNegative ? num.Insert(0, "-") : num;
+
+            // Convert our number to a workable decimal format
+            num = ToDecimal(num, fromRadix);
+
+            // If we're converting to decimal, all done
+            if (toRadix == 10)
+                return isNegative ? num.Insert(0, "-") : num;
+
+            // Convert to the desired base
+            num = ToBaseN(num, toRadix);
+            
+            // Return the result
+            return isNegative ? num.Insert(0, "-") : num;
+        }
+
+        /// <summary>Converts a number to decimal.</summary>
+        private string ToDecimal(string num, int rad)
+        {
+            // Skip if already decimal
+            if (rad == 10)
+                return num;
+
+            // Split number into integral and fractional parts
+            SplitNumber(num, out string numIntegral, out string numFractional);
+
+            // Request indexed symbols
+            char[] charPool = RequestBaseNSymbols(rad);
+
+            // Convert integral part
+            double resInteger = 0.0;
+
+            if (numIntegral != "0" && !(numIntegral == "Z" && rad == 26))
+            {
+                numIntegral = numIntegral.Reverse();
+                
+                for (int i = 0; i < numIntegral.Length; i++)
+                {
+                    resInteger += Array.IndexOf(charPool, numIntegral[i]) * Math.Pow(rad, i);
+                }
+            }
+
+            // Convert fractional part
+            double resFraction = 0.0;
+
+            if (numFractional != String.Empty)
+            {
+                for (int i = 0; i < numFractional.Length; i++)
+                {
+                    resFraction += Array.IndexOf(charPool, numFractional[i]) / Math.Pow(rad, i + 1);
+                }
+            }
+
+            return (resInteger + resFraction).ToString();
+        }
+
+        /// <summary>Converts a number to base-N.</summary>
+        private string ToBaseN(string num, int rad)
+        {
+            // Split number into integral and fractional parts
+            SplitNumber(num, out string strIntegral, out string strFractional);
+
+            // Convert integral part
+            int numIntegral = Int32.Parse(strIntegral);
+            List<int> resIntegral = new List<int>();
+
+            do
+            {
+                resIntegral.Add(numIntegral % rad);
+                numIntegral /= rad;
+            }
+            while (numIntegral > 0);
+            
+            resIntegral.Reverse();
+
+            // Convert fractional part
+            double? numFractional = null;
+            List<int> resFractional = new List<int>();
+
+            if (strFractional != String.Empty)
+            {
+                numFractional = Double.Parse(String.Format("0.{0}", strFractional));
+
+                while (numFractional > 0)
+                {
+                    int n = (int)(numFractional *= rad);
+                    resFractional.Add(n);
+                    numFractional -= n;
+                }
+            }
+
+            // Assemble and send it on its way
+            char[] charPool = RequestBaseNSymbols(rad);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (int i in resIntegral)
+            {
+                sb.Append(charPool[i]);
+            }
+
+            if (numFractional.HasValue)
+            {
+                sb.Append(".");
+
+                foreach (int i in resFractional)
+                {
+                    sb.Append(charPool[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>Splits a number into integral and fractional parts.</summary>
+        private void SplitNumber(string num, out string numIntegral, out string numFractional)
+        {
+            string[] nums = num.Split('.');
+            bool isInteger = nums.Length == 1;
+
+            numIntegral = nums[0];
+            numFractional = isInteger ? String.Empty : nums[1];
+        }
+
+        /// <summary>Validates user input.<summary>
+        public bool IsValidInput(string num, int rad)
+        {
+            // Request valid symbols for specified radix
+            char[] validChars = RequestBaseNSymbols(rad);
+            
+            // Make sure each symbol in the number is valid for the specified radix
+            foreach (char c in num)
+            {
+                if (c == '-' || c == '.')   // Negative sign & decimal point valid despite not being in validChars: skip
+                    continue;               // RegEx will guard against having more than one
+                
+                if (Array.IndexOf(validChars, c) == -1)   // If current symbol is not found
+                    return false;                         // in validChars, number is invalid
+            }
+            
+            // Alpha-numerical sequence with optional negative sign / decimal part
+            Regex numberFormat = new Regex(@"^-?[A-Z\d]+(\.[A-Z\d]+)?$");
+            
+            return numberFormat.IsMatch(num);
+        }
+
+        /// <summary>Generates an indexed list of symbols for a base numeral system.</summary>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        public char[] RequestBaseNSymbols(int rad)
+        {
+            // User input is checked by WinForms, invalid radix here means something is wrong!
+            if (!IsValidRadix(rad))
+            {
+                throw new ArgumentOutOfRangeException("Invalid radix passed to RequestBaseNSymbols()");
+            }
+
+            // Create the array containing all symbols
+            int unicodeOffset09 = 48, unicodeOffsetAZ = 65;
+            IEnumerable<int> numRange = Enumerable.Range(unicodeOffset09, 10);      // 0-9 (10 Total)
+            IEnumerable<int> abcRange = Enumerable.Range(unicodeOffsetAZ, 26);      // A-Z (26 Total)
+
+            char[] charPool = numRange.Concat(abcRange).Select(x => (char)x).ToArray();
+
+            // Return a corresponding set of symbols
+            if (rad == 26)
+            {
+                charPool[9] = 'Z';
+                return charPool.Skip(9).Take(26).ToArray();
+            }
+            else
+            {
+                return charPool.Take(rad).ToArray();
+            }
+        }
+
+        /// <summary>Checks the validity of a radix.</summary>
+        private bool IsValidRadix(int rad)
+        {
+            return (rad < 2 || rad > 36) ? false : true;
+        }   
+    }
+
+    static class StringExtensions
+    {
+        /// <summary>Reverses a string.</summary>
+        public static string Reverse(this string str)
+        {
+            return new string(str.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
--- CMB.BaseConversion/CMB.BaseConversion.WPF/MainWindow.xaml.cs
+++ CMB.BaseConversion/CMB.BaseConversion.WPF/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +22,7 @@ namespace CMB.BaseConversion.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Constructor
         public MainWindow()
         {
             InitializeComponent();
@@ -49,7 +51,23 @@ namespace CMB.BaseConversion.WPF
         // Side Bar Tabs
         private void ResetTab_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("<Placeholder> Reset Clicked </Placeholder>");
+            // Reset InputNumber
+            SetNumberPlaceholder("number", textDimmed, false);
+
+            // Clear InputBaseIn
+            InputBaseIn.SelectedIndex = -1;
+            InputBaseInPlaceholder.Visibility = Visibility.Visible;
+
+            // Clear InputBaseOut
+            InputBaseOut.SelectedIndex = -1;
+            InputBaseOutPlaceholder.Visibility = Visibility.Visible;
+
+            // Hide Result
+            ResultContainer.Visibility = Visibility.Hidden;
+
+            // Update Status Bar
+            SetStatus("Form reset");
+            PingStatusBar(Status.Flash);
         }
 
         private void ExitTab_Click(object sender, RoutedEventArgs e)
@@ -64,18 +82,15 @@ namespace CMB.BaseConversion.WPF
         }
 
         // Number Field Behaviour
-        bool isInitialisedNumber = false;
-        private SolidColorBrush textActive = new SolidColorBrush(Color.FromRgb(116, 203, 211));
-        private SolidColorBrush textDimmed = new SolidColorBrush(Color.FromRgb( 35,  86, 110));
+        private bool isInitialisedNumber = false;
+        private SolidColorBrush textActive = new SolidColorBrush(Color.FromRgb(69, 230, 233));
+        private SolidColorBrush textDimmed = new SolidColorBrush(Color.FromRgb(35,  86, 110));
 
         private void InputNumber_GotFocus(object sender, RoutedEventArgs e)
         {
             if (!isInitialisedNumber)
             {
-                InputNumber.Text = String.Empty;
-                InputNumber.Foreground = textActive;
-                InputNumber.CaretBrush = textActive;
-                isInitialisedNumber = true;
+                SetNumberPlaceholder(String.Empty, textActive, true);
             }
         }
 
@@ -83,38 +98,132 @@ namespace CMB.BaseConversion.WPF
         {
             if (InputNumber.Text == String.Empty)
             {
-                InputNumber.Text = "number";
-                InputNumber.Foreground = textDimmed;
-                isInitialisedNumber = false;
+                SetNumberPlaceholder("number", textDimmed, false);
             }
         }
 
+        private void SetNumberPlaceholder(string str, SolidColorBrush scb, bool init)
+        {
+            InputNumber.Text = str;
+            InputNumber.Foreground = scb;
+            isInitialisedNumber = init;
+        }
+
         // BaseIn/Out Field Behaviour
-        private void InputBase_DropDownOpened(object sender, EventArgs e)
-        {
-            ToggleVisibility();
-        }
-
-        private void InputBase_DropDownClosed(object sender, EventArgs e)
-        {
-            ToggleVisibility(false);
-        }
-
-        private void ToggleVisibility(bool fade = true)
+        private void InputBase_DropDown(object sender, EventArgs e)
         {
             DoubleAnimation da = new DoubleAnimation
             {
-                To = fade ? 0.1 : 1.0,
+                To = (ConvertInput.Opacity == 1.0) ? 0.1 : 1.0,
                 Duration = new Duration(TimeSpan.FromMilliseconds(300))
             };
 
             ConvertInput.BeginAnimation(OpacityProperty, da);
         }
 
+        private void InputBaseIn_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InputBaseInPlaceholder.Visibility = Visibility.Hidden;
+        }
+
+        private void InputBaseOut_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            InputBaseOutPlaceholder.Visibility = Visibility.Hidden;
+        }
+
         // Convert Button Behaviour
+        BaseConversion bc = new BaseConversion();
+
         private void ConvertInput_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("<Placeholder> Convert Clicked </Placeholder>");
+            string num = InputNumber.Text.Trim(' ').ToUpper();
+            bool isInitialisedBaseIn = Int32.TryParse(ExtractBaseDigits(InputBaseIn.Text), out int baseIn);
+            bool isInitialisedBaseOut = Int32.TryParse(ExtractBaseDigits(InputBaseOut.Text), out int baseOut);
+
+            // Validate user input
+            if (!isInitialisedNumber)
+            {
+                SetStatus("Please enter a number to be converted.");
+                PingStatusBar(Status.Warning);
+            }
+            else if (!isInitialisedBaseIn)
+            {
+                SetStatus("Please specify the number's base/radix.");
+                PingStatusBar(Status.Warning);
+            }
+            else if (!isInitialisedBaseOut)
+            {
+                SetStatus("Please specify the base/radix to convert the number to.");
+                PingStatusBar(Status.Warning);
+            }
+            else if (!bc.IsValidInput(num, baseIn))
+            {
+                string invalidText = String.Format("Invalid input. Valid symbols (base-{0}): {1}", baseIn.ToString(), GetSymbols());
+                SetStatus(invalidText);
+                PingStatusBar(Status.Error);
+            }
+            else // Send data to result fields
+            {
+                ResultNumberIn.Content = num;
+                ResultBaseIn.Content = String.Format("(Base-{0})", baseIn.ToString());
+                ResultNumberOut.Content = bc.Convert(num, baseIn, baseOut);
+                ResultBaseOut.Content = String.Format("(Base-{0})", baseOut.ToString());
+                ResultContainer.Visibility = Visibility.Visible;
+
+                SetStatus("Conversion successful");
+                PingStatusBar(Status.Flash);
+            }
+        }
+
+        private string GetSymbols()
+        {
+            int baseIn = Int32.Parse(ExtractBaseDigits(InputBaseIn.Text));
+            char[] charPool = bc.RequestBaseNSymbols(baseIn);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in charPool)
+            {
+                sb.Append(c + ", ");
+            }
+
+            sb.Length -= 2;
+            return sb.Append(".").ToString();
+        }
+
+        private string ExtractBaseDigits(string str)
+        {
+            return Regex.Match(str, "(\\d+)").Groups[1].Value;
+        }
+
+        // Status Bar Behaviour
+        private void SetStatus(string status)
+        {
+            StatusBarLabel.Content = status;
+        }
+
+        private void PingStatusBar(Status type)
+        {
+            DoubleAnimation da = new DoubleAnimation
+            {
+                From = 1.0, To = 0.0,
+                Duration = new Duration(TimeSpan.FromMilliseconds(700)),
+                //AutoReverse = true
+            };
+
+            switch(type)
+            {
+                case Status.Flash: StatusFlash.BeginAnimation(OpacityProperty, da); break;
+                case Status.Warning: StatusWarning.BeginAnimation(OpacityProperty, da); break;
+                case Status.Error: StatusError.BeginAnimation(OpacityProperty, da); break;
+                default: StatusFlash.BeginAnimation(OpacityProperty, da); break;
+            }
+        }
+
+        private enum Status
+        {
+            Flash,
+            Warning,
+            Error
         }
     }
 }
